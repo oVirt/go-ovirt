869c775d5f54ae19ecef63d2c9ec011c403e77ea
GooVirtRobot@TravisCI <travis@travis-ci.org>
2017-09-09 02:23:55 +0000

    Generator commit ID: 738387a with message: Merge pull request #79 from imjoey/add_create_vm_examples
    Add the add_vm/add_vm_disk exmaples. Travis build: 154.

---
+++ examples/add_vm.go
@@ -0,0 +1,69 @@
+//
+// Copyright (c) 2017 Joey <majunjiev@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/imjoey/go-ovirt"
+)
+
+func main() {
+	inputRawURL := "https://10.1.111.229/ovirt-engine/api"
+
+	conn, err := ovirtsdk4.NewConnectionBuilder().
+		URL(inputRawURL).
+		Username("admin@internal").
+		Password("qwer1234").
+		Insecure(true).
+		Compress(true).
+		Timeout(time.Second * 10).
+		Build()
+	if err != nil {
+		fmt.Printf("Make connection failed, reason: %v\n", err)
+		return
+	}
+	defer conn.Close()
+
+	// Get the reference to the "vms" service
+	vmsService := conn.SystemService().VmsService()
+
+	// Use the "Add" method to create a new virtual machine:
+	resp, err := vmsService.Add().
+		Vm(
+			ovirtsdk4.NewVmBuilder().
+				Name("myvm").
+				Cluster(
+					ovirtsdk4.NewClusterBuilder().
+						Name("mycluster").
+						MustBuild()).
+				Template(
+					ovirtsdk4.NewTemplateBuilder().
+						Name("Blank").
+						MustBuild()).
+				MustBuild()).
+		Send()
+
+	if err != nil {
+		fmt.Printf("Failed to add vm, reason: %v\n", err)
+		return
+	}
+	if vm, ok := resp.Vm(); ok {
+		fmt.Printf("Add vm (%v) successfully\n", vm.MustName())
+	}
+}
---
+++ examples/add_vm_disk.go
@@ -0,0 +1,92 @@
+//
+// Copyright (c) 2017 Joey <majunjiev@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/imjoey/go-ovirt"
+)
+
+func main() {
+	inputRawURL := "https://10.1.111.229/ovirt-engine/api"
+
+	conn, err := ovirtsdk4.NewConnectionBuilder().
+		URL(inputRawURL).
+		Username("admin@internal").
+		Password("qwer1234").
+		Insecure(true).
+		Compress(true).
+		Timeout(time.Second * 10).
+		Build()
+	if err != nil {
+		fmt.Printf("Make connection failed, reason: %v\n", err)
+		return
+	}
+	defer conn.Close()
+
+	// Locate the virtual machines service and use it to find the virtual machine
+	vmsService := conn.SystemService().VmsService()
+	vmsResp, _ := vmsService.List().Search("name=myvm").Send()
+	vmSlice, _ := vmsResp.Vms()
+	vm := vmSlice.Slice()[0]
+
+	// Locate the service that manages the disk attachments of the virtual machine
+	dasService := vmsService.VmService(vm.MustId()).DiskAttachmentsService()
+
+	// Use the `Add` method of the disk attachments service to add the disk. Note that the size of the disk,
+	// the `provionedSize` attribute, is specified in bytes, so to create a disk of 10 GiB the value should
+	// be 10 * 2^30.
+	resp, err := dasService.Add().
+		Attachment(
+			ovirtsdk4.NewDiskAttachmentBuilder().
+				Disk(
+					ovirtsdk4.NewDiskBuilder().
+						Name("mydisk").
+						Description("My disk description").
+						Format(ovirtsdk4.DISKFORMAT_COW).
+						ProvisionedSize(int64(10) * int64(1<<30)).
+						// TODO: issue #49, append by the vararg way
+						// StorageDomains().
+						MustBuild()).
+				Interface(ovirtsdk4.DISKINTERFACE_VIRTIO).
+				Bootable(false).
+				Active(true).
+				MustBuild()).
+		Send()
+	if err != nil {
+		fmt.Printf("Faild to add disk attachment to vm, reason: %v\n", err)
+		return
+	}
+
+	if da, ok := resp.Attachment(); ok {
+		// Wait till the disk is OK
+		disksService := conn.SystemService().DisksService()
+		diskService := disksService.DiskService(da.MustDisk().MustId())
+		for {
+			time.Sleep(5 * time.Second)
+			diskResp, _ := diskService.Get().Send()
+			if disk, ok := diskResp.Disk(); ok {
+				if disk.MustStatus() == ovirtsdk4.DISKSTATUS_OK {
+					break
+				}
+			}
+		}
+	}
+
+}
