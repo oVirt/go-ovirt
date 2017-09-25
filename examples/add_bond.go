a02c99e379f853d43f6580ea460cc46d223185f7
GooVirtRobot@TravisCI <travis@travis-ci.org>
2017-09-25 07:29:44 +0000

    Generator commit ID: 34a9a2b with message: Merge pull request #85 from imjoey/add_more_examples_continued
    Continue to add some examples. Travis build: 174.

---
+++ examples/add_bond.go
@@ -0,0 +1,109 @@
+//
+// Copyright (c) 2017 Joey <majunjiev@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/imjoey/go-ovirt"
+)
+
+func main() {
+	inputRawURL := "https://10.1.111.229/ovirt-engine/api"
+
+	conn, err := ovirtsdk4.NewConnectionBuilder().
+		URL(inputRawURL).
+		Username("admin@internal").
+		Password("qwer1234").
+		Insecure(true).
+		Compress(true).
+		Timeout(time.Second * 10).
+		Build()
+	if err != nil {
+		fmt.Printf("Make connection failed, reason: %v\n", err)
+		return
+	}
+	defer conn.Close()
+
+	// Find the service that manages the collection of hosts
+	hostsService := conn.SystemService().HostsService()
+
+	// Find the host
+	listResp, err := hostsService.List().Search("name=myhost").Send()
+	if err != nil {
+		fmt.Printf("Failed to search host list, reason: %v\n", err)
+		return
+	}
+	hostSlice, _ := listResp.Hosts()
+	host := hostSlice.Slice()[0]
+
+	// Find the service that manages the hosts
+	hostService := hostsService.HostService(host.MustId())
+
+	// Configure the host adding a bond with two slaves, and attaching it to a network with an static IP address
+	setupNetworkResp, err := hostService.SetupNetworks().
+		ModifiedBondsOfAny(
+			ovirtsdk4.NewHostNicBuilder().
+				Name("bond0").
+				Bonding(
+					ovirtsdk4.NewBondingBuilder().
+						OptionsOfAny(
+							*ovirtsdk4.NewOptionBuilder().
+								Name("mode").
+								Value("1").
+								MustBuild(),
+							*ovirtsdk4.NewOptionBuilder().
+								Name("miimon").
+								Value("100").
+								MustBuild()).
+						SlavesOfAny(
+							*ovirtsdk4.NewHostNicBuilder().
+								Name("eth0").
+								MustBuild(),
+							*ovirtsdk4.NewHostNicBuilder().
+								Name("eth1").
+								MustBuild()).
+						MustBuild()).
+				MustBuild()).
+		ModifiedNetworkAttachmentsOfAny(
+			ovirtsdk4.NewNetworkAttachmentBuilder().
+				Network(
+					ovirtsdk4.NewNetworkBuilder().
+						Name("mynetwork").
+						MustBuild()).
+				IpAddressAssignmentsOfAny(
+					*ovirtsdk4.NewIpAddressAssignmentBuilder().
+						AssignmentMethod(
+							ovirtsdk4.BOOTPROTOCOL_STATIC).
+						Ip(
+							ovirtsdk4.NewIpBuilder().
+								Address("192.168.122.100").
+								Netmask("255.255.255.0").
+								MustBuild()).
+						MustBuild()).
+				MustBuild()).
+		Send()
+	if err != nil {
+		fmt.Printf("Failed to setup network for host-%v, reason: %v\n", host.MustId(), err)
+		return
+	}
+
+	// After modifying the network configuration it is very important to make it persistent
+	hostService.CommitNetConfig().Send()
+
+}
---
+++ examples/add_floating_disk.go
@@ -0,0 +1,86 @@
+//
+// Copyright (c) 2017 Joey <majunjiev@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/imjoey/go-ovirt"
+)
+
+func main() {
+	inputRawURL := "https://10.1.111.229/ovirt-engine/api"
+
+	conn, err := ovirtsdk4.NewConnectionBuilder().
+		URL(inputRawURL).
+		Username("admin@internal").
+		Password("qwer1234").
+		Insecure(true).
+		Compress(true).
+		Timeout(time.Second * 10).
+		Build()
+	if err != nil {
+		fmt.Printf("Make connection failed, reason: %v\n", err)
+		return
+	}
+	defer conn.Close()
+
+	// Get the reference to the disks service
+	disksService := conn.SystemService().DisksService()
+
+	// Add the disk. Note that the size of the disk, the `provisionedSize` attribute, is specified in bytes,
+	// so to create a disk of 10 GiB the value should be 10 * 2^30.
+	addDiskResp, err := disksService.Add().
+		Disk(
+			ovirtsdk4.NewDiskBuilder().
+				Name("mydisk").
+				Description("My disk").
+				Format(ovirtsdk4.DISKFORMAT_COW).
+				ProvisionedSize(int64(10) * int64(1<<30)).
+				StorageDomainsOfAny(
+					ovirtsdk4.NewStorageDomainBuilder().
+						Name("mydata").
+						MustBuild()).
+				MustBuild()).
+		Send()
+	if err != nil {
+		fmt.Printf("Faild to add disk, reason: %v\n", err)
+		return
+	}
+
+	// Get disk from response
+	if disk, ok := addDiskResp.Disk(); ok {
+		// Wait till the disk is completely created
+		diskService := disksService.DiskService(disk.MustId())
+		for {
+			time.Sleep(5 * time.Second)
+			getResp, err := diskService.Get().Send()
+			if err != nil {
+				fmt.Printf("Faild to get disk status, reason: %v\n", err)
+				continue
+			}
+			// Check if the disk status is OK
+			if d, ok := getResp.Disk(); ok {
+				if d.MustStatus() == ovirtsdk4.DISKSTATUS_OK {
+					break
+				}
+			}
+		}
+	}
+
+}
